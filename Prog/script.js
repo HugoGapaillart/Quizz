3a4d5699901b5033a87676fa4bb892825b74d020
gapaillart <gapaillart@SIO.FR>
2021-02-08 14:32:22 +0100

    suite java et json

--- Prog/script.js
+++ Prog/script.js
@@ -2,16 +2,30 @@ fetch("quiz.json")
     .then(function(reponse) { return reponse.json();})
     .then(function(donne) {
         
-        const question = document.querySelector(".question");
-        const reponse1 = document.querySelector(".reponse1");
-        const reponse2 = document.querySelector(".reponse2");
-        const reponse3 = document.querySelector(".reponse3");
-        const repone4 = document.querySelector(".reponse4");
+        function choixquest(){
+            if(bouton1 == 1){
+                return Math.random(1, 2)
+            }
+        }
 
-        document.querySelector('.question').innerHTML = donne.quiz.question1.question;
-        document.querySelector('.reponse1').innerHTML = donne.quiz.question1.reponse1;
-        document.querySelector('.reponse2').innerHTML = donne.quiz.question1.reponse2;
-        document.querySelector('.reponse3').innerHTML = donne.quiz.question1.reponse3;
-        document.querySelector('.reponse4').innerHTML = donne.quiz.question1.reponse4;
+            const question = document.querySelector(".quest1");
+    
+            document.querySelector('.quest1').innerHTML = donne.quiz.question1.question;
+        }
+       /* const question = [];
+        for(let i = 0; i < donne.quiz.question.legth)
+        
+        rep = [];
 
-    });
+        document.querySelector('.bouton1').addEventListener('onclick', () => {
+            let min = 0;
+            let max = question.length;
+            let random = Math.floor(Math.random() * (max - min) + min);
+            if (question.length == 0){
+                return "youou";
+            }else{
+                document.querySelector('.quetion').innerHTML = question[random];
+            }
+        });*/
+          
+);
